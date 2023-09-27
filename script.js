b4d7e496b3e5fe6724fe4d3edea920b9c103ee7d
Vedprakash321 <131158289+Vedprakash321@users.noreply.github.com>
2023-09-28 02:42:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,35 +1,24 @@
-//your code here!
-// myList
-document.addEventListener("DOMContentLoaded", function () {
-    const myList = document.getElementById("infi-list");
-    const totalItems = 10; // Number of initial list items
-    const itemsToAdd = 8; // Number of items to add when reaching the end
 
-    // Function to add a list item
-    function addListItem() {
-        const li = document.createElement("li");
-        li.textContent = `List item ${myList.children.length + 1}`;
-        myList.appendChild(li);
-    }
-
-    // Add initial list items
-    for (let i = 0; i < totalItems; i++) {
-        addListItem();
-    }
-
-    // Event listener for scrolling to the end of the list
-    myList.addEventListener("scroll", function () {
-        const scrollHeight = myList.scrollHeight;
-        const scrollTop = myList.scrollTop;
-        const clientHeight = myList.clientHeight;
-
-        if (scrollHeight - scrollTop === clientHeight) {
-            // User has scrolled to the end, add more items
-            for (let i = 0; i < itemsToAdd; i++) {
-                addListItem();
-            }
-        }
-    });
+const scrollContainer = documnet.getElementById("infi-list");
+let i = 1;
+function addOneItem(){
+	const li = documnet.createElement("li");
+	li.innerText=`Item ${++i}`;
+	scrollContainer.appendChild(li);
+	
+	
+}
+for(let i=0;i<10;i++){
+	addOneItem();
+	
+}
+scrollContainer.addEventListener("scroll", () => {
+	const maxScrollHeight = scrollContainer.scrollHeight - scrollContainer.clientHeight;
+	let currrentScrollTop = scrollContainer.scrollTop;
+	let threshold = Math.abs(maxScrollHeight - currrentScrollTop)
+	if(threshold <= 5){
+		for(let i=0;i<2;i++){
+			addOneItem();
+		}
+	}
 });
-
-
